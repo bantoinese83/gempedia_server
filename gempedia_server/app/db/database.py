7c728947b8315dc5838525ef85027adcb7307f6f
Bryan  Antoine <110068642+bantoinese83@users.noreply.github.com>
2024-04-15 01:45:50 -0400

    Update database.py
    Get the database URL from environment variables

--- gempedia_server/app/db/database.py
+++ gempedia_server/app/db/database.py
@@ -1,12 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://root:letmeinnow@localhost/gempedia_db"
+# Load environment variables from .env file
+load_dotenv()
 
+# Get the database URL from environment variables
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+# Create the database engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+# Function to get a database session
 def get_db():
     db = SessionLocal()
     try:
